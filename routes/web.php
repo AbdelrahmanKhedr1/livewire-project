4e032ca186f2936503fc8a7a8f7f309417baab9b
AbdELrahman <abdelrahmankhed123@gmail.com>
2024-04-14 18:52:44 +0200

    Admin Theam Integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Front\FrontController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -14,13 +15,18 @@ Route::prefix('/')->name('front.')->group(function () {
     Route::get('/teatimonial', [FrontController::class, 'teatimonial'])->name('teatimonial');
 });
 
+Route::prefix('/admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+
+});
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
